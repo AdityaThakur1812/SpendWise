64a8bb4e414e08696d7c51470ad0bf56b4594b2e
Aditya Thakur <adityathakur@Adityas-MacBook-Air.local>
2026-05-22 02:28:01 +0530

    save to local Storage

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const title = document.getElementById("title");
 const amount = document.getElementById("amount");
 const type = document.getElementById("type");
 const transactionList = document.getElementById("transactionList");
+const balance = document.getElementById('balance');
+const income = document.getElementById('income');
+const expense = document.getElementById('expense');
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let transactions = [];
+loadTransactions();
 
 transactionForm.addEventListener("submit", submit);
 
@@ -15,13 +20,16 @@ function submit(e) {
   let transactionType = type.value;
 
   const transaction = {
+    id : Date.now(),
     title: transactionTitle,
-    amount: transactionAmount,
+    amount: Number(transactionAmount),
     type: transactionType,
   };
 
   transactions.push(transaction);
   renderTransactions();
+  updateSummary();
+  saveTransactions();
   title.value = "";
   amount.value = "";
 }
@@ -39,6 +47,7 @@ function renderTransactions() {
         </div>
         <div>
           <span>RS ${t.amount}</span>
+          <button class = "delete-btn" onclick ="deleteTransaction(${t.id})">Delete</Button>
         </div>`;
     if (t.type === "Income") {
       transactionItem.style.borderRight = "5px solid lime";
@@ -48,3 +57,51 @@ function renderTransactions() {
     transactionList.appendChild(transactionItem);
   });
 }
+
+function updateSummary(){
+  let totalIncome= 0;
+  let totalExpense = 0 ;
+  transactions.forEach((t) =>{
+    if(t.type === "Income"){
+      totalIncome +=t.amount;
+    }else{
+      totalExpense += t.amount;
+    }
+  });
+
+  let totalBalance = totalIncome - totalExpense;
+  balance.textContent = `RS ${totalBalance}`;
+  expense.textContent = `RS ${totalExpense}`;
+  income.textContent = `RS ${totalIncome}`;
+}
+
+function deleteTransaction(id){
+  transactions = transactions.filter((t) =>{
+    return t.id !== id;
+  });
+  renderTransactions();
+  saveTransactions();
+  updateSummary();
+}
+
+function saveTransactions(){
+  localStorage.setItem("transaction" , JSON.stringify(transactions));
+}
+
+function loadTransactions(){
+  storedTransactions = localStorage.getItem("transaction");
+  if(storedTransactions){
+    transactions = JSON.parse(storedTransactions);
+  }
+  renderTransactions();
+  updateSummary();
+}
+
+clearAllBtn.addEventListener("click", () => {
+  if(confirm("Delete all transactions?")){
+    transactions = [];
+    saveTransactions();
+    renderTransactions();
+    updateSummary();
+}   
+});
